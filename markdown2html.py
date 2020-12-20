0d0fc273932c8e2ac8166cc8295029450cf01dc8
Viet Tran <vietnamtran@gmail.com>
2020-12-20 02:18:42 +0000

    Delete newline

--- markdown2html.py
+++ markdown2html.py
@@ -5,12 +5,12 @@ from sys import argv, stderr
 
 if __name__ == "__main__":
     if len(argv) < 2:
-        stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        stderr.write("Usage: ./markdown2html.py README.md README.html")
         exit(1)
 
     
     if not path.isfile(argv[1]):
-        stderr.write("Missing {}\n".format(argv[1]))
+        stderr.write("Missing {}".format(argv[1]))
         exit(1)
 
     exit(0)
