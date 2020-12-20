85d037b1ae940fef8fafc65d24a26f42cd16834a
Viet Tran <vietnamtran@gmail.com>
2020-12-20 02:17:29 +0000

    Look only for md

--- markdown2html.py
+++ markdown2html.py
@@ -8,10 +8,9 @@ if __name__ == "__main__":
         stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
 
-    for f in argv:
-        f = './' + f
-        if not path.isfile(f):
-            stderr.write("Missing {}\n".format(f))
-            exit(1)
+    
+    if not path.isfile(argv[1]):
+        stderr.write("Missing {}\n".format(argv[1]))
+        exit(1)
 
     exit(0)
