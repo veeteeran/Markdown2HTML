e12a4b1e8ee16e3095bbf0e9f1d95518bdb5ac7a
Viet Tran <vietnamtran@gmail.com>
2020-12-20 02:09:52 +0000

    Add main guard

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Check for proper number of args"""
 
-from os import path
-from sys import argv, stderr, exit
+if __name__ == "__main__":
+    from os import path
+    from sys import argv, stderr, exit
 
-if len(argv) < 2:
-    stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-    exit(1)
-
-for f in argv:
-    f = './' + f
-    if not path.exists(f):
-        stderr.write("Missing {}\n".format(f))
+    if len(argv) < 2:
+        stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
 
-exit(0)
+    for f in argv:
+        f = './' + f
+        if not path.exists(f):
+            stderr.write("Missing {}\n".format(f))
+            exit(1)
+
+    exit(0)
