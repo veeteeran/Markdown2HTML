a668a1f77549a26d8ab52e65d02d6a54d61e4b7c
Viet Tran <vietnamtran@gmail.com>
2020-12-20 02:15:49 +0000

    Move imports

--- markdown2html.py
+++ markdown2html.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Check for proper number of args"""
+from os import path
+from sys import argv, stderr
 
 if __name__ == "__main__":
-    from os import path
-    from sys import argv, stderr, exit
-
     if len(argv) < 2:
         stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
