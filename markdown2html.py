14a07708288ad6e611dd87e2fc188d32340ae67f
Viet Tran <vietnamtran@gmail.com>
2020-12-20 02:20:09 +0000

    Check pep8

--- markdown2html.py
+++ markdown2html.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
         stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
 
-    
     if not path.isfile(argv[1]):
         stderr.write("Missing {}\n".format(argv[1]))
         exit(1)
